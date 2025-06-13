ebddb624e5820e16ca020a60f0b3a9872595d919
Zakhary Kaplan <me@zakhary.dev>
2025-06-12 20:00:00 -0400

    feat: define multiple endpoints

--- src/index.ts
+++ src/index.ts
@@ -21,20 +21,36 @@ api.use(logger())
 api.use(prettyJSON())
 
 // Define routes
-api.get("/", async (c) => {
-  return c.json(
-    await fetch("https://api.hardcover.app/v1/graphql", {
-      headers: {
-        "content-type": "application/json",
-        authorization: `Bearer ${process.env.HARDCOVER}`
-      },
-      method: "POST",
-      body: JSON.stringify({
-        query: `{
+api.get("/next", async (c) => {
+  return c.json(await upstream(Kind.Next))
+})
+api.get("/live", async (c) => {
+  return c.json(await upstream(Kind.Live))
+})
+api.get("/read", async (c) => {
+  return c.json(await upstream(Kind.Read))
+})
+
+// Define upstream fetch
+enum Kind {
+  Next = 1,
+  Live = 2,
+  Read = 3,
+}
+
+async function upstream(kind: Kind): Promise<JSON> {
+  return fetch("https://api.hardcover.app/v1/graphql", {
+    headers: {
+      "content-type": "application/json",
+      authorization: `Bearer ${process.env.HARDCOVER}`
+    },
+    method: "POST",
+    body: JSON.stringify({
+      query: `{
           me {
             user_books(
               where: {
-                status_id: {_eq: 3}
+                status_id: {_eq: ${kind}}
               }
               order_by: [
                 { last_read_date: asc },
@@ -56,15 +72,14 @@ api.get("/", async (c) => {
             }
           }
         }`
-      })
     })
-      .then(r => r.json())
-      .then(({ data }) => data.me[0].user_books.map((item: any) => ({
-        book: item.edition,
-        date: new Date(item.last_read_date),
-      })))
-  )
-})
+  })
+    .then(r => r.json())
+    .then(({ data }) => data.me[0].user_books.map((item: any) => ({
+      book: item.edition,
+      date: new Date(item.last_read_date),
+    })))
+}
 
 // Serve API
 export default {
